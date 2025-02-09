f6ae0ca0edf763a75556c005cb92ebf65edb5630
Andrey Nascimento <candreynascimento@gmail.com>
2025-02-09 19:31:06 -0300

    chore: migrate and permissions changed

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\Seller\SellerController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['prefix' => 'api'], function () {
+    Route::post('/sellers', [SellerController::class, 'store']);
 });
